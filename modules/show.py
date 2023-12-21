7a5ae679646ee166d1dbbcda38b8eeab80c3e91d
OrlyIdeate <ueno.jin25@outlook.jp>
2023-12-22 02:38:32 +0900

    add: db_list関数を作成

--- modules/show.py
+++ modules/show.py
@@ -12,37 +12,45 @@ load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 
+
+def db_list():
+
+    """DBから全情報を取得します。
+    """
+
+    config = {
+        'user': 'root',
+        'password': 'citson-buzrit-4cyxZu',
+        'host': '35.223.243.48',
+        'database': 'test1',
+        }
+    db_connection = mysql.connector.connect(**config)
+    cursor = db_connection.cursor()
+    query = "SELECT content, url, date FROM phese4;"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    similarity_list = []
+    for content, url, date in rows:
+        similarity_list.append((content, url, date))
+    response_text = "*保存されているデータ*:\n\n"
+
+    for content, url, date in similarity_list:
+        response_text += f"*Content:* {content}\n"
+        response_text += f"*URL:* <{url}|Link>\n"
+        response_text += f"*Date:* {date}\n\n"
+    return response_text
+
+
 def select_all_db(app: App):
     @app.message("@show")
     def get_db_data(message, say):
         slack_client = WebClient(token=SLACK_BOT_TOKEN)
         message_channel = message['channel']
         message_thread_ts = message['ts']
-        config = {
-        'user': 'root',
-        'password': 'citson-buzrit-4cyxZu',
-        'host': '35.223.243.48',
-        'database': 'test1',
-        }
-        db_connection = mysql.connector.connect(**config)
-        cursor = db_connection.cursor()
-        query = "SELECT content, url, date FROM phese4;"
-        cursor.execute(query)
-        rows = cursor.fetchall()
 
-        similarity_list = []
-        print(similarity_list)
-        for content, url, date in rows:
-            # 以前はここでvectorを処理していましたが、今は不要なので削除
-            similarity_list.append((content, url, date))
-        response_text = "*保存されているデータ*:\n\n"
 
-        for content, url, date in similarity_list:
-            response_text += f"*Content:* {content}\n"
-            response_text += f"*URL:* <{url}|Link>\n"
-            response_text += f"*Date:* {date}\n\n"
-
-        print(response_text)
+        response_text = db_list() # phese4テーブルから全情報を取得する
 
         try:
         # スレッド内に返信を送信
