c9686df6c0d6990599d470ae77bf8f21bea3878b
RiRinbaru <amaoto00021234@gmail.com>
2023-12-22 02:10:21 +0900

    block kitの導入

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ from modules.chatgpt import generator_answer_gpt # @GPTでChatGPTを起動させ
 from modules.search import search
 from modules.save import save
 from modules.message import message
+from modules.kit import generate_slack_message
+
+from slack_sdk import WebClient
+import json
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
@@ -27,6 +31,20 @@ generator_answer_gpt(app) # @GPTでChatGPTを起動
 search(app)
 save(app)
 message(app)
+
+slack_client = WebClient(token=SLACK_BOT_TOKEN)
+slack_message = generate_slack_message()
+response = slack_client.chat_postMessage(
+    channel="C067ALJLXRQ",
+    blocks=slack_message["blocks"]
+)
+print("起動しました")
+
+@app.action("send_button-action")
+def handle_send_button_action(ack, body, logger):
+    input_text = body["view"]["state"]["values"]["block_id"]["plain_text_input-action"]["value"]
+    logger.info(f"入力された文字列: {input_text}")
+    ack()
 
 # アプリを起動します
 if __name__ == "__main__":
---
+++ modules/kit.py
@@ -0,0 +1,81 @@
+import os
+from slack_bolt import App
+from slack_sdk.web import WebClient
+import openai
+
+# .env読み込み
+from dotenv import load_dotenv
+load_dotenv()
+
+def generate_slack_message():
+    return {
+	    "blocks": [
+		    {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "こんにちは！私はSlackGPTです！あなたが疑問に思っていることを社内で共有しませんか？私が質問を蓄積し、これまでに似た質問があればお答えいたします！"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input",
+                    "multiline": True,
+                    "action_id": "plain_text_input-action"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "質問入力欄"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "送信"
+                        },
+                        "action_id": "send_button-action"
+                    }
+                ]
+            },
+		    {
+			    "type": "actions",
+			    "elements": [
+			    	{
+			    		"type": "button",
+			    		"text": {
+			    			"type": "plain_text",
+			    			"text": "Button 1"
+			    		},
+			    		"value": "button_1",
+			    		"action_id": "actionId-1"
+			    	},
+			    	{
+			    		"type": "button",
+			    		"text": {
+			    			"type": "plain_text",
+			       			"text": "Button 2"
+			    		},
+			    		"value": "button_2",
+		    			"action_id": "actionId-2"
+		    		},
+		    		{
+		    			"type": "button",
+		    			"text": {
+		    				"type": "plain_text",
+					        "text": "Button 3"
+				        },
+				        "value": "button_3",
+				        "action_id": "actionId-3"
+			        }
+			    ]
+		    }
+	    ]
+    }
