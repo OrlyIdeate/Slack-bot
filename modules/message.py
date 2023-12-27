f607b96a0c8edd97cc9690a9ce0d4eb52bb91b8c
yuyuuyuu <fujiyoshi.yusuke.671@s.kyushu-u.ac.jp>
2023-12-27 12:59:19 +0900

    messageの修正、GPTへの質問改善

--- modules/message.py
+++ modules/message.py
@@ -20,10 +20,11 @@ def message(app):
         thread_count_query = f"SELECT message_count FROM thread_monitoring WHERE thread_id = '{thread_ts}'"
         thread_count_result = execute_query(thread_count_query)
 
-        if thread_count_result is not None:
-            thread_count = thread_count_result[0]
-
+        if thread_count_result and len(thread_count_result) > 0:
+            # リストが空でない場合、最初の要素を取得
+            thread_count = thread_count_result[0][0]  # 最初の要素の最初のカラム
         else:
+            # リストが空の場合、新しいスレッドカウントを挿入
             insert_query = f"INSERT INTO thread_monitoring (thread_id, message_count) VALUES ('{thread_ts}', 1)"
             execute_query(insert_query)
             thread_count = 1
@@ -31,14 +32,14 @@ def message(app):
         update_query = f"UPDATE thread_monitoring SET message_count = message_count + 1 WHERE thread_id = '{thread_ts}'"
         execute_query(update_query)
 
-        if thread_count % 10 == 0:
+        if thread_count % 15 == 0:
             #これまでの会話ログとスレッドの最初の質問を取得
             prev_message = client.conversations_replies(channel=message_channel, ts=thread_ts)
             ques = prev_message['messages'][0]['text']
             all_message= ""
             for i in range(len(prev_message['messages'])):
                 all_message+=prev_message['messages'][i]['text']
-            message_text_with_instruction = all_message + "\nここまでの文言は以下の質問に対する直近の会議内容です。\n" + ques + "\n質問から会議がそれてきていると考えるならば「それています」とのみ出力し、それていない場合は「それていません」とのみ出力してください。"
+            message_text_with_instruction = all_message + "\nここまでの文言は以下の質問に対する直近の会議内容です。\n" + ques + "\n質問から会議がものすごくそれてきていると考えるならば「それています」とのみ出力し、それていない場合は「それていません」とのみ出力してください。"
 
             response_text = chatgpt(message_text_with_instruction)
             if "それています" in response_text:
@@ -46,11 +47,5 @@ def message(app):
                 client.chat_postMessage(
                     channel=message_channel,
                     text="話の内容が質問内容からそれていませんか?",
-                    thread_ts=thread_ts
-                )
-            else:
-                client.chat_postMessage(
-                    channel=message_channel,
-                    text=thread_count,
                     thread_ts=thread_ts
                 )
