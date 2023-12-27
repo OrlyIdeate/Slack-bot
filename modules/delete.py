3d1a76e8251e4ad05d49c8aee64ab4984952be3c
OrlyIdeate <ueno.jin25@outlook.jp>
2023-12-27 09:16:41 +0900

    refactor: 関数の汎用性の向上

--- modules/delete.py
+++ modules/delete.py
@@ -1,11 +1,18 @@
-def del_message(client, body):
+import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+
+load_dotenv()
+client = WebClient(os.getenv("SLACK_BOT_TOKEN"))
+
+def del_message(channel_id, ts):
     """メッセージを削除します。
 
     引数:
-        client
-        body
+        channel_id: チャンネルID
+        ts: 削除したいメッセージのタイムスタンプ
     """
     client.chat_delete(
-                channel=body["container"]["channel_id"],
-                ts=body["container"]["message_ts"]
+                channel=channel_id,
+                ts=ts
             )
