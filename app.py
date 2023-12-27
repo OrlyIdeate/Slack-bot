34a65c478d2f97c382f5efad24d5b253e7da406e
OrlyIdeate <ueno.jin25@outlook.jp>
2023-12-27 10:49:02 +0900

    refactor: 全体的にリファクタ

--- app.py
+++ app.py
@@ -1,47 +1,36 @@
 import os
-import openai
 
 # Slackライブラリ
 from slack_bolt import App
-from slack_sdk.web import WebClient
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # .env読み込み
 from dotenv import load_dotenv
 load_dotenv()
 
-# 日付を文字列に変換
-from datetime import datetime
-
 # modulesフォルダのユーザー定義関数をインポート
-from modules.modal import register_modal_handlers # Slackのフォームの関数
-from modules.chatgpt import generator_answer_gpt # @GPTでChatGPTを起動させる関数
+from modules.modal import register_modal_handlers
+from modules.chatgpt import generator_answer_gpt
 from modules.search import look_for
 from modules.show import select_all_db
-from modules.save import store_thread
 from modules.message import message
 from modules.workflow import workflow_step
 from modules.upload import source
 from modules.active import thread_monitor
 
-from slack_sdk import WebClient
-import json
-
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
-thread_monitor(app)
-source(app)
-look_for(app)
-store_thread(app)
-select_all_db(app)
-register_modal_handlers(app) # Slackのフォーム
-generator_answer_gpt(app) # @GPTでChatGPTを起動
-message(app)
-workflow_step(app)
+thread_monitor(app) # 質問に対する結論が出たかどうかを調べる
+source(app) # @uploadでデータをDBに保存
+look_for(app) # @searchで検索
+select_all_db(app) # @showで全データ取得
+register_modal_handlers(app) # モーダル起動
+generator_answer_gpt(app) # メンションでChatGPTに質問
+message(app) # スレッド内の話題がそれていないかを検閲
+workflow_step(app) # ワークフローからモーダルを開くためのメッセージを送信
 
 # アプリを起動します
 if __name__ == "__main__":
