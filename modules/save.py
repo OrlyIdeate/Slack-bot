ea8e9d9016a57f5dfafd38714706adb2ca7ffcf4
OrlyIdeate <ueno.jin25@outlook.jp>
2023-12-27 09:43:11 +0900

    delete: メッセージをリスニングしてスレッドを保存する関数を削除

--- modules/save.py
+++ modules/save.py
@@ -1,101 +1,7 @@
-import logging
-logging.basicConfig(level=logging.ERROR)
-
-import os
-import openai
-from slack_bolt import App
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-from datetime import datetime
-import mysql.connector
-import pickle
-
+# 環境変数読込
 from dotenv import load_dotenv
 load_dotenv()
-
-from modules.similarity import get_embedding
-from modules.active import active_end
-from modules.chatgpt import chatgpt
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-
-def store_thread(app: App):
-    @app.message("@save")
-    def get_thread_url(event, say):
-        openai_client = openai.OpenAI(api_key=OPENAI_API_KEY)
-        slack_client = WebClient(token=SLACK_BOT_TOKEN)
-        message_channel = event['channel']
-        message_thread_ts = event.get('thread_ts', event['ts'])
-
-        # スレッド内の全会話を取得
-        prev_message = slack_client.conversations_replies(channel=message_channel, ts=message_thread_ts)
-        all_messages = " ".join(msg['text'] for msg in prev_message['messages'])
-
-        # 要約を求めるプロンプト作成
-        summary_prompt = all_messages + "\n\nこの会話の要約し、課題と結論をできるだけ短くまとめてください。"
-
-        # GPTへ要約を問い合わせ
-        summary_completion = openai_client.chat.completions.create(
-            messages=[{"role": "user", "content": summary_prompt}],
-            model="gpt-4"
-        )
-        summary_text = summary_completion.choices[0].message.content
-
-        # 要約をSlackに投稿
-        response = slack_client.chat_postMessage(
-            channel=message_channel,
-            text="会話の要約: " + summary_text,
-            thread_ts=message_thread_ts
-        )
-
-        first_message_response = slack_client.conversations_replies(channel=message_channel, ts=message_thread_ts)
-        first_message_text = first_message_response['messages'][0]['text']
-
-        # 要約を求めるプロンプト作成
-        summary_prompt = first_message_text + "\n\nこの内容をタイトル風にして。"
-
-        # GPTへ要約を問い合わせ
-        summary_completion = openai_client.chat.completions.create(
-            messages=[{"role": "user", "content": summary_prompt}],
-            model="gpt-4"
-        )
-        summary_text = summary_completion.choices[0].message.content
-
-        # スレッドのURLを生成
-        # スレッドのURLを生成
-        thread_url = f"https://dec-ph4-hq.slack.com/archives/{message_channel}/p{message_thread_ts.replace('.', '')}?thread_ts={message_thread_ts.replace('.', '')}&cid={message_channel}"
-
-
-        config = {
-        'user': 'root',
-        'password': 'citson-buzrit-4cyxZu',
-        'host': '35.223.243.48',
-        'database': 'test1',
-        }
-        db_connection = mysql.connector.connect(**config)
-        cursor = db_connection.cursor()
-        text1 = summary_text # 取得してきた質問
-        vector1 = get_embedding(text1)
-        vector_bytes = pickle.dumps(vector1)
-
-        url = thread_url
-        category = "スレッド"
-
-        today = datetime.now()
-
-        # データベースにデータを挿入
-        insert_query = "INSERT INTO phese4 (content, vector, url, date, category) VALUES (%s, %s, %s, %s, %s);"
-        cursor.execute(insert_query, (text1, vector_bytes, url ,today, category))
-        db_connection.commit()
-
-        response = slack_client.chat_postMessage(
-            channel=message_channel,
-            text="保存された内容:\n " + summary_text,
-            thread_ts=message_thread_ts  # スレッドのタイムスタンプを指定
-        )
-
-        active_end(message_thread_ts)
+from modules.chatgpt import chatgpt # Chat-GPTの回答を生成
 
 def get_thread_title(client, channel_id, thread_ts):
     """
@@ -116,7 +22,7 @@ def get_thread_title(client, channel_id, thread_ts):
     summary_prompt = all_messages + "\nこの会話に50文字以内でタイトルをつけて" # 最初のメッセージを基に要約を求めるプロンプト作成
     thread_title = chatgpt(summary_prompt) # スレッドのタイトルを生成
 
-    return thread_title
+    return thread_title # スレッドのタイトル
 
 def get_thread_summary(client, channel_id, thread_ts):
     """
@@ -137,4 +43,4 @@ def get_thread_summary(client, channel_id, thread_ts):
     summary_prompt = all_messages + "\nこの会話の内容をまとめた要約と一番最初の質問をまとめた質問と質問に対する最終的な回答である結論をそれぞれ100字以内にまとめて。" # 全会話を基に要約を求めるプロンプト作成
     summary_text = chatgpt(summary_prompt) # GPTへ要約を問い合わせ
 
-    return summary_text
+    return summary_text # 要約、質問、結論をカンマ区切りでまとめた文字列
